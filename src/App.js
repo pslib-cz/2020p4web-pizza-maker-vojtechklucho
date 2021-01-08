ec6f2b4e558cf5c7c48fcfec4b156140f31fba8f
vojtechklucho <vojtech.klucho@pslib.cz>
2021-01-08 11:48:17 +0100

    small changes

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
-import logo from './logo.svg';
-import './App.css';
 
+import './App.css';
 import { BrowserRouter as Router } from "react-router-dom";
 import { Switch, Route } from "react-router";
 import Ingredients from "./components/Ingredients";
@@ -8,17 +7,22 @@ import Calzone from "./components/Calzone";
 import Pizza from "./components/Pizza";
 import Order from "./components/Order";
 import NotFound from "./components/NotFound";
+import Home from "./components/Home";
+import Navigation from "./components/Navigation";
 
 
 function App() {
   return (
     <div className="App">
       <Router>
+          <Navigation/>
           <h1>Order pizza</h1>
           <Switch>
+            <Route exact path="/" component={Home}/>
             <Route exact path="/order/pizza" component={Pizza} />
             <Route exact path="/order/calzone" component={Calzone} />
             <Route exact path="/ingredients" component={Ingredients} />
+            <Route path="/" component={NotFound}/>
           </Switch>
       </Router>
     </div>
